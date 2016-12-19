91a63027c22fb3144c0eafc9c4d2b56456bb6ee6
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-12-18 17:37:47 -0800

    Add collision detection testing

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -3,19 +3,40 @@ module Tests exposing (..)
 import Test exposing (..)
 import Expect
 import String
-import App
+import App exposing (Player, Barrel, hasCollided)
 
 
 all : Test
 all =
-    describe "A Test Suite"
-        [ test "Addition" <|
-            \() ->
-                Expect.equal (3 + 7) 10
-        , test "String.left" <|
-            \() ->
-                Expect.equal "a" (String.left 1 "abcdefg")
-        , test "This test should fail" <|
-            \() ->
-                Expect.fail "failed as expected!"
+    describe "Game"
+        [ describe "A Test Suite"
+            [ test "Addition" <|
+                \() ->
+                    Expect.equal (3 + 7) 10
+            , test "String.left" <|
+                \() ->
+                    Expect.equal "a" (String.left 1 "abcdefg")
+            ]
+        , describe "collision detection"
+            [ test "collision" <|
+                \() ->
+                    let
+                        player =
+                            Player ( 2, 3 ) ( 0, 0 ) 1
+
+                        barrel =
+                            Barrel ( 3, 2 ) (pi / 4) 1
+                    in
+                        Expect.true "Expected collision" (hasCollided player barrel)
+            , test "collision" <|
+                \() ->
+                    let
+                        player =
+                            Player ( 7, 4 ) ( 0, 0 ) 1
+
+                        barrel =
+                            Barrel ( 9, 3 ) (pi / 4) 1
+                    in
+                        Expect.false "Expected no collision" (hasCollided player barrel)
+            ]
         ]
