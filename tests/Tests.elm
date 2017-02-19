f71aaafd884ecb3631b5b28f9f68640e7e223134
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2017-02-19 15:01:11 -0800

    Test touch collision

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -3,7 +3,10 @@ module Tests exposing (..)
 import Test exposing (..)
 import Expect
 import String
-import App exposing (Player, Barrel, hasCollided)
+import App exposing (..)
+import GameTypes exposing (Player, Barrel)
+import GameLogic exposing (hasPlayerCollided, touchIsCollidingWithBarrel)
+import Screens.LevelCreationScreen exposing (areAnyBarrelsInTheWay)
 
 
 all : Test
@@ -27,7 +30,7 @@ all =
                         barrel =
                             Barrel ( 3, 2 ) (pi / 4) 1
                     in
-                        Expect.true "Expected collision" (hasCollided player barrel)
+                        Expect.true "Expected collision" (hasPlayerCollided player barrel)
             , test "collision" <|
                 \() ->
                     let
@@ -37,6 +40,44 @@ all =
                         barrel =
                             Barrel ( 9, 3 ) (pi / 4) 1
                     in
-                        Expect.false "Expected no collision" (hasCollided player barrel)
+                        Expect.false "Expected no collision" (hasPlayerCollided player barrel)
+            ]
+        , describe "Game Logic Helper Functions"
+            [ test "touch colliding with barrel" <|
+                \() ->
+                    touchIsCollidingWithBarrel ( 3, 2 ) (Barrel ( 3, 2 ) 0 2)
+                        |> Expect.true "Expected a barrel in the way"
+            , test "touchs colliding with barrels" <|
+                \() ->
+                    let
+                        touches =
+                            [ ( 3, 2 ), ( 7, 4 ) ]
+
+                        barrelOne =
+                            Barrel ( 4, 2 ) 0 2
+
+                        barrelTwo =
+                            Barrel ( 0, 0 ) 0 2
+
+                        barrels =
+                            [ barrelOne, barrelTwo ]
+                    in
+                        Expect.true "Expected barrels in the way" (areAnyBarrelsInTheWay touches barrels)
+            , test "no collisions" <|
+                \() ->
+                    let
+                        touches =
+                            [ ( 4, 7 ), ( 7, 4 ) ]
+
+                        barrelOne =
+                            Barrel ( 20, 20 ) 0 2
+
+                        barrelTwo =
+                            Barrel ( 30, 30 ) 0 2
+
+                        barrels =
+                            [ barrelOne, barrelTwo ]
+                    in
+                        Expect.false "Expected no barrels in the way" (areAnyBarrelsInTheWay touches barrels)
             ]
         ]
